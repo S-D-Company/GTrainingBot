a8358a04f34a9297b0314e411e89cc9e2f2cf93a
Vyachelav Paltsev <fate6002@gmail.com>
2025-06-26 02:02:07 +0300

    Adding Gradle

---
+++ src/main/java/com/GTrainingBot/PumpHorse/GTrainingBotApplication.java
@@ -0,0 +1,13 @@
+package com.GTrainingBot.PumpHorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTrainingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTrainingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GTrainingBot/PumpHorse/GTrainingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GTrainingBot.PumpHorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GTrainingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
